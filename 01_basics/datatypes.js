8f497e86ede4581f7ea0b4d49b0a1290596829f8
Swara-29 <pudkeswara29@gmail.com>
2024-10-15 18:56:28 +0000

    config

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") we are using node.js , not browser
+
+console.log(3+3)
+
+let name = "swara"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol => unique
+//object
+
+console.log(typeof age)
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12344
+let accountEmail = "swara@gmail.com"
+var accountPassword = "2345"
+accountCity = "Raipur"
+accountEmail = "ss@gmail.com"
+accountCity = "Durg"
+//accountId = 2
+console.log(accountId);
+console.log(accountId);
+console.log(accountId);
+
+/*Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail ,accountId, accountCity]);
