66772fadfdcf7a6a580837f55a14d8bdb883967d
Swara-29 <pudkeswara29@gmail.com>
2024-11-24 14:52:47 +0000

    config

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+// const myObject= {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swift: 'swifvt by apple'
+// }
+
+// for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+   
+// }
+
+// const programing = ["js", "rb", "py", "java", "cpp"]
+
+// for(const key in programing) {
+//     console.log(programing[key]);
+// }
+
+// const map = new Map()
+// map.set('IN', "india")
+// map.set('USA', "united states of america")
+// map.set('FR', "france")
+
+// for(const key in map) {
+//     console.log(key);
+// }
+
