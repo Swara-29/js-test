97690f393e46802a5a0f47dfe7506491e937a65e
Swara-29 <pudkeswara29@gmail.com>
2024-11-26 12:44:49 +0000

    config

---
+++ 07_projects/number.js
@@ -0,0 +1,37 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subm');
+const userInput = document.querySelector('.guessField');
+const guessSlot = document.querySelector('.guesses');
+const lowOrHi = document.querySelector('.lowOrHi');
+const statOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let predvGuess = [];
+let numGuess = 1;
+let playGame = true;
+
+function validateGuess (guess){
+
+}
+
+function checkGuess (guess) {
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message) {
+
+}
+
+function endGame(){
+
+}
+
+function nweGame(){
+    
+}
